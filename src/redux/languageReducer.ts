ead7475a7f8dabfd598fa9dc42dc2e30b9433576
Seven <lxw_623@163.com>
2023-03-13 09:55:42 +0800

    feat: 创建state-createStore

---
+++ src/redux/languageReducer.ts
@@ -0,0 +1,19 @@
+
+interface LanguageState {
+  language: 'zh' | 'en'
+  languageList: { name: string, code: string }[]
+}
+
+const defaultState: LanguageState = {
+  language: 'zh',
+  languageList: [
+    { name: '中文', code: 'zh' },
+    { name: 'English', code: 'en' },
+  ]
+}
+
+const reducer = (state = defaultState, action) => {
+  return state
+}
+
+export default reducer
